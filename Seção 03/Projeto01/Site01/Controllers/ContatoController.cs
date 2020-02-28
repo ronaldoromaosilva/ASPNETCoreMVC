c7e8cdc2faf117e9492811500e782949b30a18dd
Ronaldo Romão <ronaldo.romao.silva@hotmail.com>
2020-02-28 00:13:39 -0300

    MVC - Modelo - Controlador - Formulário - Validação

--- Seção 03/Projeto01/Site01/Controllers/ContatoController.cs
+++ Seção 03/Projeto01/Site01/Controllers/ContatoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Site01.Models;
 
 namespace Site01.Controllers
 {
@@ -12,5 +13,33 @@ namespace Site01.Controllers
         {
             return View();
         }
-    }
+
+        public IActionResult ReceberContato([FromForm]Contato contato)
+        {
+            string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+
+            return new ContentResult() { Content = conteudo };
+        }
+
+        /* Obter dados Manualmente
+
+        public IActionResult ReceberContato()
+        {
+            //POST - Request.Form
+            //Get - Request.QueryString
+
+            Contato contato = new Contato();
+
+            contato.Nome = Request.Form["nome"];
+            contato.Email = Request.Form["email"];
+            contato.Assunto = Request.Form["assunto"];
+            contato.Mensagem = Request.Form["mensagem"];
+
+            string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+
+            return new ContentResult() { Content = conteudo};
+        }
+
+    */
+    } 
 }
---
+++ Seção 03/Projeto01/Site01/Models/Contato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Models
+{
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+    }
+}

ade4f3e2708930ef024f16b179bdde323dcf72c9
Ronaldo Romão <ronaldo.romao.silva@hotmail.com>
2020-03-04 22:24:47 -0300

    MVC - Formulário de Login

--- Seção 03/Projeto01/Site01/Controllers/HomeController.cs
+++ Seção 03/Projeto01/Site01/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Site01.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,41 @@ namespace Site01.Controllers
     {
         public ActionResult Index()
         {
-
-            // return new ContentResult() { Content = "Olá Mundo!", ContentType = "text/json" };
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromForm]Usuario usuario)
+        {
+            if (ModelState.IsValid) {
+
+                if(usuario.Email == "teste@teste.com" && usuario.Senha == "123456")
+                {
+                    return Redirect("/palavra");
+                    //return View();
+                }
+
+                else
+                {
+                    ViewBag.Mensagem = "Os dados informados são inválidos!";
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+
+           
+            
+        }
 
         
-        }
     }
 }
